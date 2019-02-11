32daf8813c13a6f69582299e82f653ec7633191b
ramen.sh@gmail.com <ramen.sh@gmail.com>
2019-02-11 19:48:38 +0800

    add operator overload

--- class_construct.cpp
+++ class_construct.cpp
@@ -21,34 +21,40 @@ class Student{
 private:
 	int sex; 	
 public:
-	Student(){
+	Student()
+	{
 		DEBUG_OUTPUT_LINE;	
 	}
-	Student(int sex){
+	Student(int sex)
+	{
 		DEBUG_OUTPUT_LINE
 		this->sex = sex;
 	}
-	Student(int sex,int age){
+	Student(int sex,int age)
+	{
 		DEBUG_OUTPUT_LINE
 		this->sex = sex;
 		this->age = age;
 	}
 #if 1
 #else	
-	Student(int sex,int age,int chinese){
+	Student(int sex,int age,int chinese)
+	{
 		DEBUG_OUTPUT_LINE
 		this->sex = sex;
 		this->age = age;
 		this->chinese = chinese;
 	}
-	Student(int sex,int age,int chinese,int maths){
+	Student(int sex,int age,int chinese,int maths)
+	{
 		DEBUG_OUTPUT_LINE
 		this->sex = sex;
 		this->age = age;
 		this->chinese = chinese;
 		this->maths = maths;
 	}
-	Student(int sex,int age,int chinese,int maths,int english){
+	Student(int sex,int age,int chinese,int maths,int english)
+	{
 		DEBUG_OUTPUT_LINE
 		this->sex = sex;
 		this->age = age;
@@ -58,25 +64,31 @@ public:
 	}
 #endif	
 	int age;
-	int getSex(){
+	int getSex()
+	{
 		DEBUG_OUTPUT_LINE;
 		return this->sex;
 	}
-	int setAge(int age){
+	int setAge(int age)
+	{
 		this->age = age;
 	}
-	int setSex(int sex){
+	int setSex(int sex)
+	{
 		this->sex=sex;
 	}
 #if 1
 	int subject[SUBJECT_MAX];
-	void setSubject(int _score,int _subject){
+	void setSubject(int _score,int _subject)
+	{
 		subject[_subject] = _score;
 	}
-	int getSubject(int _subject){
+	int getSubject(int _subject)
+	{
 		return subject[_subject];
 	}
-	int getTotalScore(){
+	int getTotalScore()
+	{
 		DEBUG_OUTPUT_LINE;
 		int sum = 0;
 		for (int i = 0;i < SUBJECT_MAX;i++)
@@ -87,47 +99,79 @@ public:
 	}
 #else	
 	int chinese;
-	int setChinese(int chinese){
+	int setChinese(int chinese)
+	{
 		this->chinese = chinese;
 	}
-	int getChinese(){
+	int getChinese()
+	{
 		return this->chinese;
 	}
 	int maths;
-	int setMaths(int maths){
+	int setMaths(int maths)
+	{
 		this->maths = maths;
 	}
-	int getMaths(){
+	int getMaths()
+	{
 		return maths;
 	}
 	int english;
-	int setEnglish(int english){
+	int setEnglish(int english)
+	{
 		this->english = english;
 	}
-	int getEnglish(){
+	int getEnglish()
+	{
 		return english;
 	}
 
 	int totalScore;
-	int setTotalScore(){
+	int setTotalScore()
+	{
 		this->chinese = chinese;
 		this->maths = maths;
 		this->english = english;
 	}
-	int getTotalScore(){
+	int getTotalScore()
+	{
 		DEBUG_OUTPUT_LINE;
 		return chinese + maths + english;
 	}
 #endif		
-	int sum(int a,int b){
+	int sum(int a,int b)
+	{
 		return a+b;
 	}
-	double sum(double a, double b){
+	double sum(double a, double b)
+	{
 		return a+b;
+	}
+	int operator++(int)
+	{
+		DEBUG_OUTPUT_LINE;
+		age--;
+	}
+	int operator--(int)
+	{
+		DEBUG_OUTPUT_LINE;
+		age -= 2;
+	}
+	int operator++()
+	{
+		age += 3;
+	}
+	int operator+=(int a)
+	{
+		this->age += a;
+	}
+	int operator+=(Student a){
+		this->age += a.age;
 	}
 };
 int main(){              
-	Student Bob(SEX_WOMAN);
+	Student Bob(SEX_WOMAN,10);
+
 	Student max(SEX_MAN,12);
 	cout << "Max's sex is " << ((max.getSex()==SEX_MAN)?"Boy":"Girl")<<" Age is "<<max.age << endl;
 #if 1
@@ -146,5 +190,16 @@ int main(){
 	cout << "Max's endlish is " << max.english<< endl;
 	cout << "Max's total score is " << max.getTotalScore()<< endl;
 #endif	
+	max++;
+	cout << "Max's sex is " << ((max.getSex()==SEX_MAN)?"Boy":"Girl")<<" Age is "<<max.age << endl;
+	max--;
+	cout << "Max's sex is " << ((max.getSex()==SEX_MAN)?"Boy":"Girl")<<" Age is "<<max.age << endl;
+	++max;
+	cout << "Max's sex is " << ((max.getSex()==SEX_MAN)?"Boy":"Girl")<<" Age is "<<max.age << endl;
+	max+=1;//==>max.+=(1);
+	cout << "Max's sex is " << ((max.getSex()==SEX_MAN)?"Boy":"Girl")<<" Age is "<<max.age << endl;
+	max+=Bob;//==>max.+=(Bob);
+	cout << "Max's sex is " << ((max.getSex()==SEX_MAN)?"Boy":"Girl")<<" Age is "<<max.age << endl;
+	
 }
 
